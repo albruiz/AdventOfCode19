06d8abca400de1865fc674d902f25a8717cbf026
unknown <a.ruizandres99@gmail.com>
2019-12-07 18:14:24 +0100

    terminado

--- Exercise6.py
+++ Exercise6.py
@@ -4,23 +4,23 @@
 def getRectas(listaPuntos):
     verticales = []
     horizontales = []
+
     for i in range (len(listaPuntos)):
         x = listaPuntos[i][0]
         y = listaPuntos[i][1]
 
         for j in range (i, len(listaPuntos)):
-            if(x != listaPuntos[j][0] and y == listaPuntos[j][1]):
-                pass
-            elif x == listaPuntos[j][0] and y != listaPuntos[j][1]:
-                listaIntermedia = []
-                listaIntermedia.append(listaPuntos[i])
-                listaIntermedia.append(listaPuntos[j])
-                verticales.append(listaIntermedia)
-            if y == listaPuntos[j][1] and x != listaPuntos[j][0]:
+            if x != listaPuntos[j][0] and y == listaPuntos[j][1] and j < i + 2:
                 listaIntermedia = []
                 listaIntermedia.append(listaPuntos[i])
                 listaIntermedia.append(listaPuntos[j])
                 horizontales.append(listaIntermedia)
+            elif x == listaPuntos[j][0] and y != listaPuntos[j][1] and j < i + 2:
+                listaIntermedia = []
+                listaIntermedia.append(listaPuntos[i])
+                listaIntermedia.append(listaPuntos[j])
+                verticales.append(listaIntermedia)
+
     listaRectas = [verticales, horizontales]
     return listaRectas
 
@@ -42,9 +42,7 @@ def getIntersecciones(rectas1, rectas2):
             x1 = j[0][0]
             x2 = j[1][0]
             if ((x1 < x and x2 > x) or (x1 > x and x > x)) and ((y1 < y and y < y2 ) or (y1 > y and y2 < y )):
-                listaIntersecciones.append([x , y])
-                listaIntersecciones.append(i)
-
+                listaIntersecciones.append([x, y])
 
     for i in verticales2:
         x = i[0][0]
@@ -55,26 +53,9 @@ def getIntersecciones(rectas1, rectas2):
             x1 = j[0][0]
             x2 = j[1][0]
             if ((x1 < x and x2 > x) or (x1 > x and x > x)) and ((y1 < y and y < y2 ) or (y1 > y and y2 < y )):
-                listaIntersecciones.append([x , y])
-                listaIntersecciones.append(i)
-
+                listaIntersecciones.append([x, y])
 
     return listaIntersecciones
-
-#parameter lista = array with the intersection points bettween the lines
-#returns distanciaMinima = min. Manhattan distance between 2 points
-def getDistanciaMin(lista):
-    distanciaMinima = 10000000000000
-    puntoBueno = []
-    for i in lista:
-        distanciaManh = abs(i[0])+abs(i[1])
-        if distanciaManh < distanciaMinima :
-            distanciaMinima = distanciaManh
-            del puntoBueno[:]
-            puntoBueno.append(i)
-
-    return puntoBueno
-
 
 def leerFichero(ruta):
     file = open(ruta, 'r')
@@ -125,18 +106,73 @@ def obtenerPuntos(movimientos):
             listaPuntos.append([puntoAnterior[0], puntoAnterior[1] + numero])
     return listaPuntos
 
+def getTraza(punto, rectas1, rectas2):
+    verticales1 = rectas1[0]
+    verticales2 = rectas2[0]
+    horizontales1 = rectas1[1]
+    horizontales2 = rectas2[1]
+    lista1 = []
+    listaFinal = []
 
-def getCoste(listaPuntosCable1, puntosRecta, movimientos):
-    indicador = listaPuntosCable1.index(puntosRecta[0])
-    coste = 0
-    for i in range(indicador -1):
-        numero = movimientos.strip(movimientos[i][0])
-        coste += numero
+    for i in verticales1:
+        x = i[0][0]
+        y1 = i[0][1]
+        y2 = i[1][1]
+        lista1.append(i[0])
+        lista2 = []
+        for j in horizontales2:
+            y = j[0][1]
+            x1 = j[0][0]
+            x2 = j[1][0]
+            lista2.append(j[0])
+            if x == punto[0] and y == punto[1]:
+                listaFinal.append(lista1)
+                listaFinal.append(lista2)
+                return listaFinal
 
-    return coste
+    lista1 = []
+    for i in verticales2:
+        x = i[0][0]
+        y1 = i[0][1]
+        y2 = i[1][1]
+        lista1.append(i[0])
+        lista2 = []
+        for j in horizontales1:
+            y = j[0][1]
+            x1 = j[0][0]
+            x2 = j[1][0]
+            lista2.append(j[0])
+            if x == punto[0] and y == punto[1]:
+                listaFinal.append(lista1)
+                listaFinal.append(lista2)
+
+                return listaFinal
+
+def calculaPeso(traza, punto, indicador):
+    sumaX = 0
+    sumaY = 0
+    anteriorx = 0
+    anteriory = 0
+    X = 0
+    Y = 0
+    for i in traza[indicador]:
+        x = anteriorx - i[0]
+        sumaX += abs(x)
+        # y =
+        y = anteriory - i[1]
+        sumaY += abs(y)
+        X = i[0]
+        Y = i[1]
+        anteriorx = i[0]
+        anteriory = i[1]
+
+    sumaX += abs(X - punto[0])
+    sumaY += abs(Y - punto[1])
+
+    return sumaX + sumaY
 
 
-#main
+
 ruta = 'text3.txt'
 movimientos = leerFichero(ruta)
 listaPuntosCable1 = obtenerPuntos(movimientos[0])
@@ -144,18 +180,12 @@ listaPuntosCable2 = obtenerPuntos(movimientos[1])
 rectasCable1 = getRectas(listaPuntosCable1)
 rectasCable2 = getRectas(listaPuntosCable2)
 puntosInterseccion = getIntersecciones(rectasCable1, rectasCable2)
-listaBuena = []
-for i in range(len(puntosInterseccion)):
-    if i % 2 == 0 :
-        listaBuena.append(puntosInterseccion[i])
-puntoBueno = getDistanciaMin(listaBuena)
-indicador = puntosInterseccion.index(puntoBueno[0])
-print(listaPuntosCable1, 'kll', puntoBueno)
-coste1 = getCoste(listaPuntosCable1, puntosInterseccion[indicador + 1], movimientos[0])
-coste2 = getCoste(listaPuntosCable2, puntosInterseccion[indicador + 1], movimientos[1])
-print(coste1, coste2)
-exit()
-#indicador +1=puntos del cable1
-#indicador +2=puntos del cable2
-exit()
-print(f'La distancia minima entre una interseccion y el punto (0,0) es: {distanciaMinima}' )
+trazaSolucion = 10000000000000
+for i in puntosInterseccion:
+    traza = getTraza(i, rectasCable1, rectasCable2)
+    peso0 = calculaPeso(traza, i, 0)
+    peso1 = calculaPeso(traza, i, 1)
+
+    if (peso1 + peso0) < trazaSolucion: trazaSolucion = peso1 + peso0
+
+print(f'La distancia recorrida desde (0,0) es: {trazaSolucion}' )
